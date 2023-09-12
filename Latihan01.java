6f8dea7aba872a227f3b953073018367cb1c7e08
sayevvv <shamilbasayev10@gmail.com>
2023-09-12 10:14:22 +0700

    commit

---
+++ Latihan01.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan01
+ */
+public class Latihan01 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Abdullah Shamil Basayev");
+       // System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ MyFirstJavaGithub01.java
@@ -0,0 +1,9 @@
+public class MyFirstJavaGithub01{
+    public static void main(String[] args) {
+        System.out.println("Nama : Abdullah Shamil Basayev");
+        System.out.println("Absen : 01");
+        System.out.println("Kelas : 1D");
+        System.out.println("Prodi : Teknik Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
